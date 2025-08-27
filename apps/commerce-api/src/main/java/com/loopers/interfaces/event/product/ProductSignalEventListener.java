265213a5dc2ae4ddb03f561158b57713c6c57d6f
jyj1143 <jyj1143@gmail.com>
2025-08-27 20:28:49 +0900

    [volume-2] 이벤트 기반 좋아요 처리 Async추가

--- apps/commerce-api/src/main/java/com/loopers/interfaces/event/product/ProductSignalEventListener.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/event/product/ProductSignalEventListener.java
@@ -5,6 +5,7 @@ import com.loopers.domain.product.dto.product.ProductEvent;
 import com.loopers.domain.like.enums.LikeType;
 import com.loopers.domain.product.service.ProductSignalService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -19,6 +20,7 @@ public class ProductSignalEventListener {
      * 상품 등록 이벤트 처리
      * - 상품 등록 시, 상품 시그널 테이블 생성
      */
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleRegisterProductSignal(ProductEvent.Register event) {
         productSignalService.register(event.productId());
@@ -28,6 +30,7 @@ public class ProductSignalEventListener {
      * 좋아요 이벤트 처리
      * - 상품 좋아요 시, 상품 신호의 좋아요 수 증가
      */
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleIncreaseLikeCount(LikeEvent.Like event) {
         if (LikeType.PRODUCT.equals(event.likeType())) {
@@ -39,6 +42,7 @@ public class ProductSignalEventListener {
      * 좋아요 취소 이벤트 처리
      * - 상품 좋아요 취소 시, 상품 신호의 좋아요 수 감소
      */
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleDecreaseLikeCount(LikeEvent.UnLike event) {
         if (LikeType.PRODUCT.equals(event.likeType())) {
