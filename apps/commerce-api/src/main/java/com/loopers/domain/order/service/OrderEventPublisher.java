5b6f0db9b8d89faa9eb5dc252894738a8a16a4e1
jyj1143 <jyj1143@gmail.com>
2025-08-27 13:07:19 +0900

    [FEAT] 주문 생성 이벤트 퍼블리셔 추가

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/order/service/OrderEventPublisher.java
@@ -0,0 +1,8 @@
+package com.loopers.domain.order.service;
+
+import com.loopers.domain.order.dto.OrderEvent;
+
+public interface OrderEventPublisher {
+
+    void publish(OrderEvent.Created event);
+}
